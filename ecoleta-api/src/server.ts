739e5bf130b88a957b973f4f2df5f9ea6cd06a8c
Bruno Moura <brunomoura.blm@gmail.com>
2020-06-06 00:47:58 -0300

    Ajustes finais na API

--- ecoleta-api/src/server.ts
+++ ecoleta-api/src/server.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
-
 import path from 'path';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -10,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
